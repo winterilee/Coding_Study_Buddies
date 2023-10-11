fbbd1251d378a18a10053fa5b6d580bb640ec992
Autumn Han <autumn9894@gmail.com>
2023-10-11 12:24:01 -0700

    initial commit for garageSale

---
+++ garageSale/garageSale/src/main/java/com/coding_dojo/garageSale/GarageSaleApplication.java
@@ -0,0 +1,13 @@
+package com.coding_dojo.garageSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageSaleApplication.class, args);
+	}
+
+}
